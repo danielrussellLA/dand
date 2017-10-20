4cc07e40363b8a17a870bc82e30e9dc7d8efdb45
danielrussellla <danielrussell16@gmail.com>
2017-10-20 07:30:11 -0700

    delete unused asset and beautify all files with 4 spaces

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
-  <div id="app">
+<div id="app">
     <router-view/>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+    name: 'app'
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,37 +1,38 @@
 <template>
-  <div class="home">
+<div class="home">
     home
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  data () {
-    return {
+    name: 'Home',
+    data() {
+        return {
 
+        }
     }
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+h1,
+h2 {
+    font-weight: normal;
 }
 
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
 
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
