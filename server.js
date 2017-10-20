02859d4e19b936192dd9fecbd1c4c41d4bbc645f
danielrussellla <danielrussell16@gmail.com>
2017-10-19 20:06:05 -0700

    added simple server and setup main components/routing

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+// middleware
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+// instantiate express app
+const app = express();
+
+// use middleware
+app.use(morgan('dev'));
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
+app.get('/', function(req, res) {
+  res.sendFile(path.resolve(__dirname + 'index.html'));
+});
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname + 'index.html'));
+});
+
+// create/run server
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(app).listen(PORT);
+console.log('listening on port ' + PORT);
