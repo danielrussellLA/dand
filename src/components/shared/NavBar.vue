7080816fabcbab121c480e8fdf8f97ec29cddb47
danielrussellla <danielrussell16@gmail.com>
2017-10-23 19:38:25 -0700

    started working on nav

--- src/components/shared/NavBar.vue
+++ src/components/shared/NavBar.vue
@@ -1,6 +1,8 @@
 <template>
 <div class="navbar">
-    Nav
+    <div class="logo">
+        
+    </div>
 </div>
 </template>
 
@@ -17,22 +19,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1,
-h2 {
-    font-weight: normal;
-}
-
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
-
-a {
-    color: #42b983;
+.navbar {
+    border-bottom: 1px solid black;
+    padding: 10px;
 }
 </style>
