568a4b0196afe5f8fd3a55cf3caefa14ae86773a
danielrussellla <danielrussell16@gmail.com>
2017-11-02 20:22:00 -0700

    working on it

--- src/components/shared/NavBar.vue
+++ src/components/shared/NavBar.vue
@@ -1,17 +1,24 @@
 <template>
 <div class="navbar">
-    <div class="logo">
-        
+    <div class="content">
+        <div class="logo">
+            <img :src="logo" />
+        </div>
+        <div class="links">
+            <router-link v-for="link in links" :key="link.path" :to="link.path" active-class="active" exact>{{ link.name }}</router-link>
+        </div>
     </div>
 </div>
 </template>
 
 <script>
+import data from '../../../schema/BudgetCalculator.json'
 export default {
     name: 'NavBar',
     data() {
         return {
-
+            logo: data.navbar.logo,
+            links: data.navbar.links
         }
     }
 }
@@ -21,6 +28,29 @@ export default {
 <style scoped>
 .navbar {
     border-bottom: 1px solid black;
+    border-top: 1px solid black;
     padding: 10px;
+    position: relative;
+}
+.content {
+    max-width: 800px;
+    margin: auto;
+    position: relative;
+}
+.logo img {
+    height: 30px;
+    width: 30px;
+}
+.links {
+    position: absolute;
+    right: 15px;
+    top: 50%;
+    margin-top: -12px;
+}
+.links a {
+    margin: 5px;
+    text-decoration: none;
+    color: #000;
+    font-weight: bold;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import Article from '@/components/Article'
+import Article from '@/components/Article/Article'
 
 Vue.use(Router)
 
@@ -13,7 +13,7 @@ export default new Router({
             component: Home
         },
         {
-            path: '/:article',
+            path: '/article/:title?', // add an optional title for specific article content
             name: 'article',
             component: Article
         },
